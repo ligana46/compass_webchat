771a6780ac7b7a895cf6b3c4153aaaf7bcfa07cb
Carlos <carlos_ruizc@msn.com>
2022-06-19 05:16:37 +0200

    Added button to open and close chat

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,1 +1,11 @@
 const myID = Math.floor(Date.now() / 1000)
+
+
+function toogleChat() {
+	var element = document.getElementById("chatContainer");
+	if (element.style.display == "none" || element.style.display == "") {
+		element.style.display = "block";
+	} else {
+		element.style.display = "none";
+	}
+}

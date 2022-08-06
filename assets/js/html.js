3599b5aa2fa8cb17a1ec2270b65156d2afd57dce
Carlos <carlos_ruizc@msn.com>
2022-08-07 01:11:32 +0200

    En el acordeon: solo se muestra el primer elemento

--- assets/js/html.js
+++ assets/js/html.js
@@ -41,41 +41,19 @@ function clearId(id) {
 	document.getElementById(id).value = "";
 }
 
-
-function generateCarrusel2(array) {
-	console.log(array)
-	// @TODO: ARREGLAR PROBLEMA DE PEDIR DOS 
-	var carrusel = '<div class="bd-example"><div id="carouselExampleCaptions' + array[0].recordIdentifier + '" class="carousel slide" data-interval="false" data-ride="carousel"><ol class="carousel-indicators"><li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li><li data-target="#carouselExampleCaptions" data-slide-to="1"></li><li data-target="#carouselExampleCaptions" data-slide-to="2"></li></ol><div class="carousel-inner">';
-
-	for (var i = 0; i < array.length; i++) {
-		if (i == 0) {
-			carrusel += '<div class="carousel-item active"><img src="https://www.macmillandictionary.com/us/external/slideshow/full/Grey_full.png" class="d-block w-100" alt="..."><div style="display: block!important;;" class="carousel-caption d-none d-md-block"><a target="_blank" href="' + array[i].link.href + '"><h5>' + array[i]["title"] + '</h5></a></div></div>';
-		} else {
-			carrusel += '<div class="carousel-item"><img src="https://www.macmillandictionary.com/us/external/slideshow/full/Grey_full.png" class="d-block w-100" alt="..."><div class="carousel-caption d-none d-md-block"><a target="_blank" href="' + array[i].link.href + '"><h5>' + array[i]["title"] + '</h5></a></div></div>';
-		}
-	}
-
-	carrusel += '<a class="carousel-control-prev" href="#carouselExampleCaptions' + array[0].recordIdentifier + '" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only"></span></a><a class="carousel-control-next" href="#carouselExampleCaptions' + array[0].recordIdentifier + '" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only"></span></a>'
-
-	carrusel += '</div></div></div>'
-	return carrusel;
-}
-
-
 function generateCarrusel(array) {
-	const idName = "accordion-" + Math.floor(Date.now() / 1000)
+	const idName = "accordion-" + Math.floor(Date.now() / 1000);
+	var show = "show";
 	console.log(array)
 	var carrusel = '<div id="' + idName + '">';
-
 	for (var i = 0; i < array.length; i++) {
-
-
+		if (i >= 1) { show = "" }
 		let item = array[i];
 		let name = item.name;
 		let recordIdentifier = item.recordIdentifier;
 		let isbn = "9788498381405"; // @TODO: CAMBIAR ESTO
 		let title = item.title;
-		let href = item.link.href
+		let href = item.link.href;
 
 		let imageURL = "https://covers.openlibrary.org/b/isbn/" + isbn + "-M.jpg";
 		carrusel += '';
@@ -88,7 +66,7 @@ function generateCarrusel(array) {
 			'\t\t\t\t\t\t\t\t\t</h5>\n' +
 			'\t\t\t\t\t\t\t\t</div>\n' +
 			'\n' +
-			'\t\t\t\t\t\t\t\t<div id="collapse' + recordIdentifier + '" class="collapse" aria-labelledby="headingOne" data-parent="#' + idName + '">\n' +
+			'\t\t\t\t\t\t\t\t<div id="collapse' + recordIdentifier + '" class="collapse ' + show + '" aria-labelledby="headingOne" data-parent="#' + idName + '">\n' +
 			'\t\t\t\t\t\t\t\t\t<div class="card-body" style="padding: 0rem 1rem;background: url(' + imageURL + ') no-repeat center #eee;">\n' +
 			'\t\t\t\t\t\t\t\t\t\t<div class="card-deck">\n' +
 			'\t\t\t\t\t\t\t\t\t\t\t<div class="card" style="background-color: rgb(255 255 255 / 85%);">\n' +
